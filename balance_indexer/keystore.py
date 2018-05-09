efd24364d3659c899ed825314a137f905905f76f
jinchung <jin.chung17@gmail.com>
2018-05-09 15:09:17 -0400

    pairing down for simple keystore

--- balance_indexer/keystore.py
+++ balance_indexer/keystore.py
@@ -5,23 +5,12 @@ ASSET_PREFIX='asset'
 PAIR_PREFIX='pair'
 
 async def initialize_keystore(loop, local=False):
-  if local:
-    return await create_connection(event_loop=loop)
-  else:
-    sentinels = sources.get_kms_parameter('balance-indexer-redis-sentinels')
-    return await create_sentinel_connection(event_loop=loop, sentinels=sentinels.split(','))
+  return await create_connection(event_loop=loop)
 
 
 async def create_connection(event_loop, host='localhost', port=6379, db=0):
   redis_uri = 'redis://{}:{}/{}'.format(host, port, db)
   return await aioredis.create_redis(address=redis_uri, db=db, encoding='utf-8', loop=event_loop)
-
-
-async def create_sentinel_connection(event_loop, sentinels):
-  default_port = 26379
-  sentinel_ports = [(x, default_port) for x in sentinels]
-  sentinel = await aioredis.create_sentinel(sentinel_ports, encoding='utf-8', loop=event_loop)
-  return sentinel
 
 
 async def add_currency_details(redis_conn, all_currencies):
